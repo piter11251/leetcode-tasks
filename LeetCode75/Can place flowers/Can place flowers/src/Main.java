a97b9ae92f3763ad4d81f065d30d6cdd67bfb4a4
thekozli <duszeq13@gmail.com>
2024-04-16 15:19:07 +0200

    flowers done

---
+++ LeetCode75/Can place flowers/Can place flowers/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        //System.out.println(solution.canPlaceFlowers(new int[] {0,0,1,0,1}, 1));
+        System.out.println(solution.canPlaceFlowers(new int[] {1,0,0,0,1,0,0}, 2));
+    }
+}
