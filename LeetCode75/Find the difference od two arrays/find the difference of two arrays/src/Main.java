c9c219985fae14a6b8b77c8ea050ca5d252d4dfd
thekozli <duszeq13@gmail.com>
2024-04-17 12:22:56 +0200

    Find the difference of two arrays

---
+++ LeetCode75/Find the difference od two arrays/find the difference of two arrays/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findDifference(new int[] {1,2,3,3}, new int[] {1,1,2,2}));
+    }
+}
