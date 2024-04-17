625ed4f950925643741133ebbad907a5bba614bd
thekozli <duszeq13@gmail.com>
2024-04-17 14:11:11 +0200

    is subsequence

---
+++ LeetCode75/Is Subsequence/Is Subsequence/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isSubsequence("abc", "ahbgdc"));
+        System.out.println(solution.isSubsequence("axc", "ahbgdc"));
+    }
+}
